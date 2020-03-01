e59cbef6a51907d2d6946d07f54e886f61bdf34b
Tom Ritchford <tom@swirly.com>
2020-03-01 14:54:07 +0100

    Rename run_list() to run_to_list()

--- run_subprocess.py
+++ run_subprocess.py
@@ -1,3 +1,4 @@
+import functools
 import itertools
 import shlex
 import subprocess
@@ -12,25 +13,43 @@ _SUBPROCESS_KWDS = {
 }
 
 
-def run(cmd, out=print, err=None, sleep=0, count=0, **kwds):
-    """Run a subprocess with separate error and output callbacks"""
-    err = err or out
+def run(cmd, out=print, err=None, sleep=0, count=None, **kwds):
+    """
+    Run a subprocess, read its stdin and stderr, and send them to error and
+    output callbacks.
 
+    Returns the integer error code that the subprocess returned.
+
+    cmd:
+        A list or tuple of strings, or a string that is split using shlex
+
+    out:
+        The callback for stdout from the subprocess
+
+    err:
+        The callback for stderr from the subprocess
+
+    sleep:
+        How long to sleep between checking the process
+
+    count:
+        Maximum number lines to retrieve at a time, from stdout or stderr
+
+    kwds:
+        Keywords that are passed to subprocess.Popen
+    """
+    err = err or out
     kwds = dict(_SUBPROCESS_KWDS, **kwds)
+
     if kwds.get('shell'):
         if not isinstance(cmd, str):
             cmd = ' '.join(cmd)
+
     elif isinstance(cmd, str):
         cmd = shlex.split(cmd)
 
     with subprocess.Popen(cmd, **kwds) as p:
-
-        def read(fp, callback):
-            for i in range(count) if count else itertools.count():
-                line = fp.readline()
-                if not line:
-                    return i
-                callback(line.decode('utf-8').rstrip('\n'))
+        read = functools.partial(read_lines, count=count)
 
         while read(p.stdout, out) or read(p.stderr, err) or p.poll() is None:
             if sleep:
@@ -39,6 +58,33 @@ def run(cmd, out=print, err=None, sleep=0, count=0, **kwds):
     return p.returncode
 
 
-def run_list(cmd, **kwds):
+def run_to_list(cmd, **kwds):
+    """
+    Redirect the stdout of a subprocess to a list, and return that list.
+
+    If the parameter `err` is not set, then error messages will also be
+    added to the list.
+
+    """
     out = []
     return run(cmd, out=out.append, **kwds), out
+
+
+def read_lines(stream, callback, count=None):
+    """
+    Reads lines of text from a stream and sends them to a callback, until the
+    stream blocks.
+
+    Returns the number of lines read.
+
+    If `count` is not None, at most `count` lines are read.
+
+    """
+    for i in itertools.count() if count is None else range(count):
+        line = stream.readline()
+        if line:
+            callback(line.decode('utf-8').rstrip('\n'))
+        else:
+            return i
+
+    return i + 1
--- test/test_run.py
+++ test/test_run.py
@@ -1,4 +1,4 @@
-from run_subprocess import run_list
+from run_subprocess import run_to_list
 import os
 import unittest
 
@@ -8,13 +8,13 @@ class RunTest(unittest.TestCase):
         os.chdir(os.path.dirname(os.path.dirname(__file__)))
 
     def test_simple(self):
-        error, lines = run_list('ls')
+        error, lines = run_to_list('ls')
         assert error == 0
         assert 'run_subprocess.py' in lines
         assert len(lines) >= 10
 
     def test_error(self):
-        error, lines = run_list('ls foo setup.py bar')
+        error, lines = run_to_list('ls foo setup.py bar')
         assert error
         assert 'setup.py' in lines
         no_such = 'No such file or directory'
