6a98a12326c573965ce0e35dfe35c7169f296389
Tom Ritchford <tom@swirly.com>
2020-04-24 12:35:39 +0200

    Pare down features, add documentation

--- subprocessor.py
+++ subprocessor.py
@@ -1,7 +1,5 @@
-import itertools
 import shlex
 import subprocess
-import time
 
 __version__ = '0.9.1'
 
@@ -12,11 +10,78 @@ _SUBPROCESS_KWDS = {
 }
 
 
-class Call:
-    def __init__(self, cmd, sleep=0, count=None, **kwds):
+class Sub:
+    def __init__(self, cmd, **kwds):
         """
-        Run a command in a subprocess, and yield is_error, line pairs.
+        An iterator yielding ``is_err, line`` pairs from stdout and stderr of
+        a process.
 
+        Starts a subprocess, and reads lines from both stdout and stderr,
+        yielding a series of ``is_err, line`` pairs, where ``is_err`` is true
+        if ``line`` came from stderr.
+
+        ARGUMENTS
+            cmd:
+                A list or tuple of strings or a string to run in a subprocess.
+
+                If shell=True, Popen expects a string, so if ``cmd`` is a list,
+                it is joined using shlex.
+
+                If shell=False, Popen expects a list of strings, so if ``cmd``
+                is a string, it is split using shlex.
+
+            kwargs:
+                Keyword arguments passed to subprocess.Popen()
+        """
+        self.kwds = dict(_SUBPROCESS_KWDS, **kwds)
+
+        self.cmd = cmd
+        if self.kwds.get('shell'):
+            if not isinstance(cmd, str):
+                self.cmd = shlex.join(cmd)
+        else:
+            if isinstance(cmd, str):
+                self.cmd = shlex.split(cmd)
+
+        self.proc = None
+
+    @property
+    def returncode(self):
+        return self.proc and self.proc.returncode
+
+    stderr_first = True
+
+    def __iter__(self):
+        with subprocess.Popen(self.cmd, **self.kwds) as self.proc:
+            data_received = True
+
+            while data_received:
+                data_received = False
+
+                for first in True, False:
+                    is_err = first == self.stderr_first
+                    stream = self.proc.stderr if is_err else self.proc.stdout
+
+                    while True:
+                        line = stream.readline()
+                        if not line:
+                            break
+
+                        data_received = True
+                        yield is_err, line.decode('utf-8').rstrip('\n')
+
+                data_received = self.proc.poll() is None or data_received
+
+
+def call(cmd, out=None, err=None, **kwargs):
+    """
+    Run a subprocess, read lines from its stdin and stderr, and send them
+    to callbacks.
+
+    Returns the shell integer error code from the subprocess, where 0 means
+    no error.
+
+    ARGUMENTS
         cmd:
             A list or tuple of strings, or a string, to run in a subprocess.
 
@@ -24,70 +89,85 @@ class Call:
             is joined using shlex.
 
             If shell=False, Popen expects a list of strings, so if ``cmd`` is a
-            string, it is split using shlex
-
-        sleep:
-            How long to sleep between checking the processes, in seconds
-
-        count:
-            Maximum number of lines to retrieve at a time from the streams
-            stdout and stderr. If count is empty, retrieve lines until the
-            stream blocks.
-
-        kwds:
-            Keywords that are passed to subprocess.Popen
-        """
-        self.kwds = dict(_SUBPROCESS_KWDS, **kwds)
-        if self.kwds.get('shell'):
-            if isinstance(cmd, str):
-                self.cmd = cmd
-            else:
-                self.cmd = shlex.join(cmd)
-        elif isinstance(cmd, str):
-            self.cmd = shlex.split(cmd)
-        else:
-            self.cmd = cmd
-        self.sleep = sleep
-        self.counter = range(count) if count else itertools.count()
-        self.returncode = None
-
-    def __iter__(self):
-        """Yield a series of ``error, line`` pairs from the subprocess cmd"""
-
-        with subprocess.Popen(self.cmd, **self.kwds) as p:
-            while True:
-                got_data = False
-                for is_error in True, False:
-                    stream = (p.stdout, p.stderr)[is_error]
-                    for i in self.counter:
-                        line = stream.readline()
-                        if not line:
-                            break
-                        got_data = True
-                        yield is_error, line.decode('utf-8').rstrip('\n')
-
-                if not (p.poll() is None or got_data):
-                    self.returncode = p.returncode
-                    break
-
-                if self.sleep:
-                    time.sleep(self.sleep)
-
-    def communicate(self, out=print, err=None):
-        """
-        Run the subprocess, read lines from stdin and stderr, and send them to
-        callbacks.
-
-        Returns the shell integer error code from the subprocess, where 0 means
-        no error.
+            string, it is split using shlex.
 
         out:
-            ``out`` is called for each line in the subprocess's stdout
+          ``out`` is called for each line from the subprocess's stdout,
+          if not None.
 
         err:
-            ``err`` is called for each line in the subprocess's stderr
-        """
-        for is_error, line in self:
-            (err if err and is_error else out)(line)
+          ``err`` is called for each line from the subprocess's stderr,
+          if not None.
 
-        return self.returncode
+        kwargs:
+            Keyword arguments passed to subprocess.Popen()
+    """
+    sub = Sub(cmd, **kwargs)
+    for is_err, line in sub:
+        if is_err:
+            err and err(line)
+        else:
+            out and out(line)
+    return sub.returncode
+
+
+def run(cmd, **kwargs):
+    """
+    Starts a subprocess ``p``, reads lines from stdout and stderr into two
+    arrays ``out`` and ``err``, then returns a triple:
+        ``out, err, p.returncode``
+
+    ARGUMENTS
+        cmd:
+            A list or tuple of strings or a string to run in a subprocess.
+
+            If shell=True, Popen expects a string, so if ``cmd`` is a list,
+            it is joined using shlex.
+
+            If shell=False, Popen expects a list of strings, so if ``cmd``
+            is a string, it is split using shlex.
+
+        kwargs:
+            Keyword arguments passed to subprocess.Popen()
+    """
+    out, err = [], []
+    error_code = call(cmd, out.append, err.append, **kwargs)
+    return out, err, error_code
+
+
+def log(cmd, out='  ', err='* ', print=print, **kwargs):
+    """
+    Runs a command as a  subprocess, reads lines from stdin and stderr, and
+    prints them.
+
+    Returns the shell integer error code from the subprocess, where 0 means
+    no error.
+
+    ARGUMENTS
+        cmd:
+            A list or tuple of strings, or a string, to run in a subprocess.
+
+            If shell=True, Popen expects a string, so if ``cmd`` is a list, it
+            is joined using shlex.
+
+            If shell=False, Popen expects a list of strings, so if ``cmd`` is a
+            string, it is split using shlex.
+
+        out:
+            Prefix for each line to stdout
+
+        err:
+            Prefix for each line to stderr
+
+        print:
+            a function that accepts individual strings
+
+        kwargs:
+            Keyword arguments passed to subprocess.Popen()
+    """
+    return call(
+        cmd,
+        out=lambda x: print(out + x),
+        err=lambda x: print(err + x),
+        **kwargs
+    )
--- test/test_run.py
+++ test/test_run.py
@@ -9,7 +9,7 @@ class RunTest(unittest.TestCase):
         self.lines = []
 
     def sub(self, cmd):
-        return subprocessor.Call(cmd).communicate(self.lines.append)
+        return subprocessor.call(cmd, self.lines.append, self.lines.append)
 
     def test_simple(self):
         error = self.sub('ls')
@@ -21,5 +21,27 @@ class RunTest(unittest.TestCase):
         error = self.sub('ls foo setup.py bar')
         assert error
         assert 'setup.py' in self.lines
-        no_such = 'No such file or directory'
-        assert sum(no_such in i for i in self.lines) == 2
+        for f in 'foo', 'bar':
+            assert sum(i.endswith(_NO_SUCH) for i in self.lines) == 2
+
+    def test_log(self):
+        error = subprocessor.log(
+            'ls foo setup.py bar', print=self.lines.append
+        )
+        assert error
+        assert '  setup.py' in self.lines
+        for f in 'foo', 'bar':
+            assert sum(f in i for i in self.lines) == 1
+            assert sum(i.startswith('*') for i in self.lines) == 2
+
+    def test_run(self):
+        out, err, error_code = subprocessor.run('ls foo setup.py bar')
+        assert error_code
+        assert 'setup.py' in out
+        assert len(err) == 2
+        for f in 'bar', 'foo':
+            assert sum(f in i for i in err) == 1
+        assert all(i.endswith(_NO_SUCH) for i in err)
+
+
+_NO_SUCH = 'No such file or directory'
