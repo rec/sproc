58c2ce31a0b984b948eb605dbf4155cf6b9b074b
Tom Ritchford <tom@swirly.com>
2020-04-26 10:55:01 +0200

    Stop supporting Python 3.4

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 
 _classifiers = [
     'Development Status :: 4 - Beta',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
